4f6cf91a8d5900b452aaf9f0890527c59daeb3f5
Abishek <167686016+AbishekVijay05@users.noreply.github.com>
2024-11-09 21:13:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>YOYO Hotel Rooms</title>
+    <title>YOYO Rooms</title>
     <link rel="stylesheet" href="style.css">
     <link rel="icon" type="image/x-icon" href="logo.png">
 </head>
